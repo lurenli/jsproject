02f63e21a462f832ce19bdf90dc258c2be31e873
web-李敏 <you@example.com>
2018-02-02 16:37:55 +0800

    php图片上传
    git commit -m php图片上传

--- haier/login/php/public.php
+++
@@ -1,9 +0,0 @@
-<?php  
-header('content-type:text/html;charset="utf-8"');
-$conn=mysql_connect('localhost','root','12345678');
-if(!$conn){
-		die('数据库链接失败'.mysql_error());
-	}
-mysql_select_db('html5');
-mysql_query('SET NAMES UTF8');
-?>
